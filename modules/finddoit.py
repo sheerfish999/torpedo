e682eab47f011c02ea6d855458e203664c448e31
unknown <gyrx-zhangxf@icbccs.local>
2017-07-27 09:28:43 +0800

    modified:   modules/finddoit.py

--- modules/finddoit.py
+++ modules/finddoit.py
@@ -162,7 +162,7 @@ def send_keys(browser,xpath, value, displayedwait=1):             # displayedwai
 
 
     # 向下滑动直到找到元素(如果有滑块)
-    browser.execute_script("arguments[0].scrollIntoView(true)",xpath)
+	browser.execute_script("arguments[0].scrollIntoView(true)",xpath)
 
 
 	## 最终用于操作的元素
