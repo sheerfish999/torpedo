122f8aca0d45f108f7900f1c41f21a4d719904b4
unknown <gyrx-zhangxf@icbccs.local>
2017-08-01 13:06:11 +0800

    modified:   modules/dodoc.py

--- modules/dodoc.py
+++ modules/dodoc.py
@@ -100,8 +100,8 @@ def openthedoc():
 	if platform.system()=="Windows":
 
 		#document=win32com.client.Dispatch('Word.Application')
-		#document=win32com.client.DispatchEx('Word.Application')   ### 独立进程，不影响其它进程
-		document=win32com.client.gencache.EnsureDispatch('Word.Application')       ### 这样可以引用 constants
+		document=win32com.client.DispatchEx('Word.Application')   ### 独立进程，不影响其它进程
+		#document=win32com.client.gencache.EnsureDispatch('Word.Application')       ### 这样可以引用 constants  ---这个不好用
 
 		document.Visible = 0        ## 默认为0    某些场景无效，原因不明
 		#document.WindowState = 2   #1表示正常，2表示最小化，3表示最大化
